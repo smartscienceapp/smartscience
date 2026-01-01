435e549662ea5aa2a45dc3b19fd8e4368919857b
smartscienceapp <smartscienceapp@gmail.com>
2026-01-01 15:42:40 +0700

    tyr update3

--- backend/app/api/v1/user/total_siswa.py
+++ backend/app/api/v1/user/total_siswa.py
@@ -9,5 +9,5 @@ router = APIRouter()
 
 @router.get("/total_siswa", response_model=dict)
 def get_total_siswa(db: Session = Depends(get_db)):
-    total_siswa = db.query(User).filter(User.is_active == True).count()
+    total_siswa = db.query(User).filter(User.is_active == True).filter(User.id_role == 2).count()
     return {"total_siswa": total_siswa}
