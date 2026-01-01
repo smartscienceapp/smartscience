ba754e03af13db5542e99c20d4c8b0644b6456cb
smartscienceapp <smartscienceapp@gmail.com>
2026-01-01 17:16:19 +0700

    Ubah ke static export

--- frontend/smartscience/next.config.ts
+++ frontend/smartscience/next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: "export",              // <--- WAJIB: Agar jadi file HTML statis
-  images: { unoptimized: true }, // <--- WAJIB: Agar gambar tidak error
-  // Tambahkan config lain di sini jika perlu
+const nextConfig: NextConfig = { 
+  images: { unoptimized: true }, 
 };
 
 export default nextConfig;
