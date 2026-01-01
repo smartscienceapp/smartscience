0de5589c56963735f9559e8e9cf657dacacad896
smartscienceapp <smartscienceapp@gmail.com>
2026-01-01 16:45:49 +0700

    fix npm ci

--- frontend/smartscience/app/users/create_user/page.tsx
+++ frontend/smartscience/app/users/create_user/page.tsx
@@ -9,6 +9,7 @@ import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/com
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Button } from "@/components/ui/button"
+
 import { cn } from "@/lib/utils"
 import {
     Command,
