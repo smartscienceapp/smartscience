afb4fa7a06b2b9ea64b7e1c578e0bd81e291aa70
smartscienceapp <smartscienceapp@gmail.com>
2026-01-02 16:48:59 +0700

    update config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -3,16 +3,14 @@ from pydantic import PostgresDsn
 
 class Setting(BaseSettings):
     PROJECT_NAME: str = "SmartScience"
-    SECRET_KEY: str = "smartsciencejuara"
+    SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 
-    DATABASE_URL: str = "postgresql://postgres.imheqybfsrapopumewya:5DZZOP42x3PJSZYB@aws-1-ap-northeast-1.pooler.supabase.com:5432/postgres"
+    DATABASE_URL: str
 
     class Config:
-        env_file = ".env"
-        # Tambahkan ini agar Pydantic tetap membaca env file meski ada error encoding
-        env_file_encoding = 'utf-8' 
-        # Opsional: Case sensitive bisa mempengaruhi pembacaan .env
+        env_file = ".env" 
+        env_file_encoding = 'utf-8'  
         case_sensitive = True
 
 settings = Setting()
